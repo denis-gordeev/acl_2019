f19318f3dc358da08e3e1337541a0061523d0f42
Denis <gordeev-d-i@yandex.ru>
2019-03-29 19:09:40 +0300

    various experiments

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import itertools
 import random
+import pickle
 import json
-
+import re
+import multiprocessing as mp
 import numpy as np
+from scipy import spatial
 import pandas as pd
+import tensorflow as tf
+from gensim.models import KeyedVectors
+
+from keras.layers import Dense, Dropout, Input
+from keras.models import Model
+from keras.callbacks import EarlyStopping
 
 from nltk import word_tokenize
 from nltk.corpus import wordnet
+import pymorphy2
+
 from scipy.optimize import linear_sum_assignment
 from sklearn.model_selection import train_test_split
-from gensim.models import KeyedVectors
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
 import networkx as nx
 from node2vec import Node2Vec
 
 from utils import read_nigp_csv_pd, get_averaged_vectors, read_okpd_pd,\
     matrices_cosine_distance, get_top_indices
-from utils_d.ml_models import CnnLstm
+from utils_d.ml_models import CnnLstm, train_lgb
 from utils_d.ml_utils import create_word_index
 
 
@@ -487,12 +500,70 @@ def main():
                 match_df = annotate(match_df, a, level, v_m)
 
 
+def get_ru_relations(w0_i, word_0, gbm, word_matrix, allowed_words, emb_norm):
+    print("\t", w0_i, end="\r")
+    emb_0 = word_matrix[w0_i]
+    # distances = 1 - spatial.distance.cosine(emb_0, emb_1)
+    distances = np.matmul(emb_0, word_matrix.T)
+    norm = emb_norm[w0_i] * emb_norm.T
+    distances = distances.T / norm
+    distances = distances[0]
+    embs_1 = [list(emb_0) + list(word_matrix[j]) + [distances[j]]
+              for j in range(len(allowed_words))]
+    embs_1 = np.array(embs_1)
+    preds = gbm.predict(embs_1)
+    max_preds = np.argmax(preds, axis=1)
+    scores = preds[np.arange(len(preds)), max_preds]
+    word_hypernyms = []
+    word_synonyms = []
+    for s_i, s in enumerate(scores):
+        if s > 0.5 and max_preds[s_i] in (0, 1, 3):
+            pred = max_preds[s_i]
+            if pred == 0:
+                word_1 = allowed_words[s_i]
+                word_hypernyms.append((word_0, word_1, s))
+                print("hyp", word_0, word_1, s)
+            elif pred == 1:
+                word_1 = allowed_words[s_i]
+                word_synonyms.append((word_0, word_1, s))
+                print("syn", word_0, word_1, s)
+            # reverse hypernyms
+            elif pred == 3:
+                word_1 = allowed_words[s_i]
+                word_hypernyms.append((word_1, word_0, s))
+                print("hyp", word_1, word_0, s)
+    return word_hypernyms, word_synonyms
+
+
+def analyse_collocations(eng_emb):
+    collocations = [w for w in eng_emb.vocab if "_" in w]
+    collocations = [c for c in collocations
+                    if all(w in eng_emb.vocab for w in c.split("_")) and
+                    len(c.split("_")) > 1]
+    col_vectors = np.array([eng_emb[c] for c in collocations])
+    print(col_vectors.shape)
+    col_vectors = np.array([eng_emb[c] for c in collocations])
+    w_vectors = [np.mean([eng_emb[w] for w in c.split("_") if w], axis=0)
+                 for c in collocations]
+    w_vectors = np.array(w_vectors)
+    # similarities; large is closer
+    sim = [1 - spatial.distance.cosine(w_vectors[w_i], col_vectors[w_i])
+           for w_i, w in enumerate(w_vectors)]
+    print(np.median(sim))
+
+
 def april_new():
     lang = "en"
     eng_emb = load_embeddings(lang)
+    # analyse_collocations(eng_emb)
+    # 117659 words
     eng_words = {w for w in wordnet.all_synsets()}
-    eng_words = {w for w in eng_words if w.name().split(".")[0] in eng_emb}
-
+    # 87943
+    eng_words = {w for w in eng_words if w.hyponyms() or w.hypernyms()}
+    # 43647 synsets; 24099 words
+    eng_words = {w for w in eng_words if
+                 w.name().split(".")[0] in eng_emb.vocab}
+    # eng_words = {w for w in eng_words if w.name().split(".")[0]}
     wordnet_dict = dict()
     for e_i, e in enumerate(eng_words):
         print("\t", e_i, end="\r")
@@ -514,10 +585,12 @@ def april_new():
                     wordnet_dict[h] = {e}
                 else:
                     wordnet_dict[h].add(e)
+    # 20378 words
     all_words = set(wordnet_dict.keys())
     for v in wordnet_dict.values():
         all_words.update(set(v))
     # random choise does not accept sets
+    # transform words into their ids
     all_words = list(all_words)
     word_index = {w: i for i, w in enumerate(all_words)}
     word_matrix = [eng_emb[w] for w in all_words]
@@ -535,6 +608,7 @@ def april_new():
     # Class 1: hyponym hyponym
     # Class 2: hypernym random_word
     #          hyponym random_word
+    # Class 3: hyponym hypernym
     # all three classes are +/- balanced
     j = 0
     for hypernym, hyponyms in wordnet_dict.items():
@@ -543,52 +617,255 @@ def april_new():
         for h in hyponyms:
             x.append((hypernym, h))
             y.append(0)
+            x.append((h, hypernym))
+            y.append(3)
         combinations = list(itertools.combinations(hyponyms, 2))
 
         # to keep the dataset balanced
         random.shuffle(combinations)
-        combinations = combinations[:len(hyponyms)]
+        combinations = combinations[:len(hyponyms) * 2]
         x += combinations
         y += [1] * len(combinations)
         for i in range(len(combinations)):
-            done = False
-            while not done:
-                random_word = random.choice(all_words)
-                random_word = word_index[random_word]
-                if random_word == hypernym or random_word in hyponyms:
-                    continue
-                else:
-                    if i <= int(len(combinations) / 2):
-                        x.append((hypernym, random_word))
-                    else:
-                        x.append((random.sample(hyponyms, 1)[0], random_word))
-                    y.append(2)
-                    done = True
+            random_words = random.sample(all_words, 10)
+            random_words = [word_index[r] for r in random_words]
+            random_words = [w for w in random_words
+                            if w not in hyponyms and w != hypernym]
+            x.append((hypernym, random_words[0]))
+            y.append(2)
+            x.append((random.sample(hyponyms, 1)[0], random_words[1]))
+            y.append(2)
+    # words to their embeddings
+    x = [[word_matrix[t] for t in row] for row in x]
+    cosine = [1 - spatial.distance.cosine(r[0], r[1]) for r in x]
+    x = [list(t[0]) + list(t[1]) + [cosine[t_i]] for t_i, t in enumerate(x)]
     x = np.array(x)
+    # x = np.array([np.concatenate(row) for row in x])
     y = np.array(y)
     x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=0.2, random_state=42)
 
-    kwargs = {
-        "voc_size": len(all_words) - 1,
-        "sequence_len": 2,
-        "vec_len": eng_emb.vector_size,
-        "categ_nums": [3],
-        "name": "w2v/",
-        "use_generator": False,
-        "x_train": x_train,
-        "x_test": x_test,
-        "y_train": y_train,
-        "y_test": y_test,
-        "embedding_matrix": word_matrix,
-        "layers_multiplier": 1,
-        "kernel_size": 1,
-        "pool_size": 1,
-        "trainable_embeddings": False
-    }
-    cnn = CnnLstm(**kwargs)
-    model = cnn.cnn_lstm_classification()
-    return model
+    # kwargs = {
+    #     "voc_size": len(all_words) - 1,
+    #     "sequence_len": 2,
+    #     "vec_len": eng_emb.vector_size,
+    #     "categ_nums": [3],
+    #     "name": "w2v/",
+    #     "use_generator": False,
+    #     "x_train": x_train,
+    #     "x_test": x_test,
+    #     "y_train": y_train,
+    #     "y_test": y_test,
+    #     "embedding_matrix": word_matrix,
+    #     "layers_multiplier": 1,
+    #     # "kernel_size": 1,
+    #     # "pool_size": 1,
+    #     "trainable_embeddings": False,
+    #     "use_embeddings": False
+    # }
+    # cnn = CnnLstm(**kwargs)
+    # model = cnn.cnn_lstm_classification()
+    gbm_params = {
+        'objective': 'multiclass',
+        'max_depth': 8,
+        'num_leaves': 12,
+        'subsample': 0.8,
+        'learning_rate': 0.1,
+        'estimators': 1000,
+        'num_trees': 10000,
+        'num_class': 4,
+        'early_stopping_rounds': 10,
+        'verbose': 0}
+    gbm, score = models, score = train_lgb(
+        x_train, x_test, y_train, y_test, k_fold=False, params=gbm_params)
+    gbm = gbm[0]
+    pickle.dump(gbm, open(f"gbm_{score}.pcl", "wb"))
+    preds = gbm[0].predict(x_test)
+    for threshold in (0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99):
+        print(threshold)
+        mask = [i for i in range(len(preds)) if any(preds[i] > threshold)]
+        mask_preds = preds[mask]
+        mask_preds = np.argmax(mask_preds, axis=1)
+        mask_y = y_test[mask]
+        print(accuracy_score(mask_y, mask_preds))
+        print(confusion_matrix(mask_y, mask_preds))
+
+    ru_emb = load_embeddings("ru")
+    morph = pymorphy2.MorphAnalyzer()
+
+    # 200000 words
+    allowed_words = list(ru_emb.vocab)
+
+    # 448
+    ru_collocations = [w for w in allowed_words if "_" in w]
+    # 297
+    ru_collocations = [w for w in ru_collocations if
+                       len([l for l in w.split("_") if l]) > 1]
+    # 146
+    ru_collocations = [w for w in ru_collocations
+                       if not re.fullmatch("[a-zA-Z_]+", w)]
+    # 108
+    ru_collocations = [w for w in ru_collocations
+                       if re.fullmatch("[ёЁа-яА-Я_-]+", w)]
+    # leave only words from opencorpora.org
+    # 139674 words; open corpora and ru_emb preprocessings are not compatible
+    # allowed_words = [w for w in allowed_words if
+    #                  morph.word_is_known(w) or any(s in w for s in "_-")]
+    # allowed_words = [w.replace("ё", "е") for w in allowed_words]
+
+    # 163569 words
+    allowed_words = [w for w in allowed_words
+                     if re.fullmatch("[а-яА-Я]+[а-яА-Я_]+[а-яА-Я]+", w)]
+
+    # morphed = [morph.parse(w)[0] for w in allowed_words]
+    # Sgtm - singularia tantum
+    # bad_tags = {"Name", "plur", "Geox", "Sgtm"}
+    # good_tags = {"NOUN", "nomn", "sing"}
+    # allowed_words = [w for w_i, w in enumerate(allowed_words)
+    #                  if not any(t in morphed[w_i].tag for t in bad_tags) and
+    #                  all(t in morphed[w_i].tag for t in good_tags)]
+    # allowed_words = [w for w in allowed_words if len(w) < 17 or "_" in w]
+    word_matrix = np.array([ru_emb[w] for w in allowed_words])
+    emb_norm = np.linalg.norm(word_matrix, axis=1)[np.newaxis].T
+
+    ru_synonyms = []
+    ru_hypernyms = []
+    # irange = [(w0_i, word_0, gbm, word_matrix, allowed_words, emb_norm)
+    #           for w0_i, word_0 in enumerate(allowed_words)]
+    # pool = mp.pool.ThreadPool(4)
+
+    for w0_i, word_0 in enumerate(allowed_words):
+        word_hypernyms, word_synonyms = get_ru_relations(
+            w0_i, word_0, gbm, word_matrix, allowed_words, emb_norm)
+        ru_hypernyms += word_hypernyms
+        ru_synonyms += word_synonyms
+    pickle.dump(ru_synonyms, open("ru_synonyms_2.pcl", "wb"))
+    pickle.dump(ru_hypernyms, open("ru_hypernyms_2.pcl", "wb"))
+    # In [27]: ru_wordnet = dict()
+    #     ...: for r in ru_hypernyms:
+    #     ...:     k, v = r
+    #     ...:     if k not in ru_wordnet:
+    #     ...:         ru_wordnet[k] = set([v])
+    #     ...:     else:
+    #     ...:         ru_wordnet[k].add(v)
+    # for w1_i, word_1 in enumerate(allowed_words):
+    #     if w0_i == w1_i:
+    #         continue
+    #     # The function is not symmetric
+    #     # if w1_i > w0_i:
+    #     #     continue
+    #     emb_1 = word_matrix[w1_i]
+    #     dist = distances[word_1]
+    #     gbm_input = np.array(list(emb_0) + list(emb_1) + [dist])
+    #     pred = gbm.predict([gbm_input])[0]
+    #     max_pred = np.argmax(pred)
+    #     score = pred[max_pred]
+    #     pred = max_pred
+    #     if score < 0.9:
+    #         continue
+
+    return None
+
+
+def tf_model(x_train, y_train, x_test, y_test):
+    """Model function for CNN."""
+    # Input Layer; concatenated embeddings
+    with tf.Session() as sess:
+        batch_size = 128
+
+        emb = tf.placeholder(shape=(None, 600), name='emb', dtype=tf.float32)
+        dense_0 = tf.layers.dense(emb, 512, activation='relu')
+        dense_1 = tf.layers.dense(dense_0, 256, activation='relu')
+        dense_2 = tf.layers.dense(dense_1, 3, activation='relu')
+        labels = tf.placeholder(shape=(None), name='labels', dtype=tf.int64)
+        output = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            labels=labels, logits=dense_2)
+
+        cost = tf.reduce_mean(output)
+
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.01).minimize(cost)
+        accuracy, update_op = tf.metrics.accuracy(
+            labels=labels, predictions=tf.argmax(dense_1, 1),
+            name='accuracy')
+
+        patience = 5
+        train_loss_results = []
+        train_accuracy_results = []
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
+
+        for epoch in range(1000):
+            epoch_loss = 0
+            epoch_acc = 0
+            epoch_range = int(len(x_train) / batch_size) + 1
+            for i in range(epoch_range):
+                batch_start = batch_size * i
+                batch_end = batch_start + batch_size
+                epoch_x = x_train[batch_start: batch_end]
+                epoch_y = y_train[batch_start: batch_end]
+                batch_opt, batch_cost, batch_acc = sess.run(
+                    [optimizer, cost, accuracy],
+                    feed_dict={emb: epoch_x, labels: epoch_y})
+                epoch_loss += batch_cost
+                epoch_acc += batch_acc
+                print(f"Epoch: {epoch}; loss: {batch_cost}; "
+                      f"acc: {batch_acc}", end="\r")
+            epoch_acc /= epoch_range
+            epoch_loss /= epoch_range
+            print(f"\nEpoch: {epoch}; loss: {epoch_loss}; "
+                  f"acc: {epoch_acc}")
+            val_range = int(len(x_test) / batch_size) + 1
+            val_loss = 0
+            acc_val = 0
+            for i in range(val_range):
+                batch_start = batch_size * i
+                batch_end = batch_start + batch_size
+                val_x = x_test[batch_start: batch_end]
+                val_y = y_test[batch_start: batch_end]
+                batch_cost, epoch_acc = sess.run(
+                    [cost, accuracy],
+                    feed_dict={emb: val_x, labels: val_y})
+                val_loss += batch_cost
+                acc_val += epoch_acc
+            val_loss /= val_range
+            acc_val /= val_range
+            if epoch == 0:
+                tf.saved_model.simple_save(sess, 'w2v/tf/my-model',
+                                           inputs={"emb": emb},
+                                           outputs={"output": output})
+            else:
+                if val_loss < train_loss_results[-1]:
+                    tf.saved_model.simple_save(sess, 'w2v/tf/my-model',
+                                               inputs={"emb": emb},
+                                               outputs={"output": output})
+                else:
+                    patience -= 1
+            train_accuracy_results.append(acc_val)
+            train_loss_results.append(val_loss)
+            print("\n", epoch_loss, "Train accuracy:",
+                  acc_val, "Val accuracy:", acc_val)
+            if patience <= 0:
+                break
+        print("training complete")
+
+
+def keras_model(x_train, y_train, x_test, y_test):
+    es = EarlyStopping(monitor='val_loss', min_delta=0, patience=5,
+                       verbose=0, mode='auto')
+    inputs = Input(shape=(601, ))
+    dropout_1 = Dropout(0.1)(inputs)
+    dense_1 = Dense(512, activation='relu')(dropout_1)
+    dropout_2 = Dropout(0.1)(dense_1)
+    dense_2 = Dense(256, activation='relu')(dropout_2)
+    outputs = Dense(3, activation='softmax')(dense_2)
+    model = Model(inputs=inputs, outputs=outputs)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',
+                  metrics=['accuracy'])
+    model.fit(x_train, y_train,
+              batch_size=128, epochs=2000,
+              validation_data=(x_test, y_test),
+              callbacks=[es])
+    model.save("w2v/keras_cnn_lstm.h5")
 
 
 if __name__ == "__main__":
